7a298baa277dd3d623faf3fb7e5c5c1a17bd2b15
Nathan Osman <admin@quickmediasolutions.com>
2014-03-04 14:21:46 -0800

    Added password reset form.

--- twobuntu/accounts/forms.py
+++ twobuntu/accounts/forms.py
@@ -1,7 +1,21 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import PasswordResetForm, UserCreationForm
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class RegistrationForm(UserCreationForm):
     """Form for new user registrations."""
 
     email = forms.EmailField(help_text="Used for verifying your account and password resets.")
+
+class ResetForm(PasswordResetForm):
+    """Form for resetting a user's password."""
+
+    email = forms.EmailField(help_text="Email address used when registering.")
+
+    def clean_email(self):
+        """Verify that the email address is valid."""
+        try:
+            return User.objects.get(email=self.cleaned_data['email'])
+        except User.DoesNotExist:
+            raise ValidationError("The email address you provided does not match any valid account.")
