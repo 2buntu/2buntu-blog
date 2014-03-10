610aea90ceb02ba22767418596827a277e397e65
Nathan Osman <admin@quickmediasolutions.com>
2014-03-09 22:39:26 -0700

    Added optional parameters to @canonical decorator and added the decorator to article views (closes #15).

--- twobuntu/articles/views.py
+++ twobuntu/articles/views.py
@@ -2,10 +2,11 @@ from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 
 from twobuntu.articles.models import Article
+from twobuntu.decorators import canonical
 
-def view(request, id, slug):
+@canonical(Article, published=True)
+def view(request, article):
     """Display the specified article."""
-    article = get_object_or_404(Article, pk=id, published=True)
     return render(request, 'articles/view.html', {
         'title':   article.title,
         'parent':  {
--- twobuntu/decorators.py
+++ twobuntu/decorators.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404, redirect
 
-def canonical(model):
+def canonical(model, **kwargs):
     """Enforce a canonical URL for a resource."""
     def outer(view):
-        def inner(request, pk, slug):
-            instance = get_object_or_404(model, pk=pk)
+        def inner(request, id, slug):
+            instance = get_object_or_404(model, pk=id, **kwargs)
             if not request.path == instance.get_absolute_url():
                 return redirect(instance, permanent=True)
             return view(request, instance)
