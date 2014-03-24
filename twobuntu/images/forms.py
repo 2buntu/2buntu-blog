844cb9b5e83b98c94b2768475d63f6d4695aee30
Nathan Osman <admin@quickmediasolutions.com>
2014-03-23 23:03:31 -0700

    Added image upload form.

---
+++ twobuntu/images/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from twobuntu.images.models import Image
+
+class ImageUploadForm(forms.ModelForm):
+    """Form for uploading images."""
+    
+    class Meta:
+        model = Image
+        fields = ('caption', 'image',)
