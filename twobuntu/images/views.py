204575af29c7eb65abf2e73d7331980d899fbc27
Nathan Osman <admin@quickmediasolutions.com>
2014-03-24 21:52:34 -0700

    Completed standard version of image upload form.

--- twobuntu/images/views.py
+++ twobuntu/images/views.py
@@ -1,13 +1,22 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from twobuntu.images.forms import ImageUploadForm
 
 def upload(request):
     """Upload images for embedding in articles."""
+    if request.method == 'POST':
+        form = ImageUploadForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            image = form.save()
+            messages.info(request, "Your image has been uploaded and can be embedded with \"[image:%d]\"." % image.id)
+            return redirect('home')
+    else:
+        form = ImageUploadForm()
     return render(request, 'images/upload.html', {
         'title': 'Upload',
-        'form':  ImageUploadForm(),
+        'form':  form,
         'description': "Use this form to upload an image.",
         'action': 'Upload',
     })
