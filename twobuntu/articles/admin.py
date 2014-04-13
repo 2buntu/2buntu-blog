7debb05ad5fd54282efd4e594ec0e3707227b489
Nathan Osman <admin@quickmediasolutions.com>
2014-04-13 16:57:50 -0700

    Limited articles that could be released under a CC license in the admin to only the author's own articles.

--- twobuntu/articles/admin.py
+++ twobuntu/articles/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 
 from twobuntu.articles.models import Article
 
-def make_released(modeladmin, request, queryset):
-    """Releases the articles under a CC BY-SA 4.0 license."""
-    queryset.update(cc_license=True)
-make_released.short_description = "Release articles under CC BY-SA 4.0 license"
-
 class ArticleAdmin(admin.ModelAdmin):
     """Describe the administration inferface for articles."""
+
+    def make_released(self, request, queryset):
+        """Releases the articles under a CC BY-SA 4.0 license."""
+        queryset.filter(author=request.user).update(cc_license=True)
+    make_released.short_description = "Release articles under CC BY-SA 4.0 license"
 
     actions = (make_released,)
     date_hierarchy = 'date'
