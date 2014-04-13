ad74bb42d88bd9aa624026390899b722b5c5791a
Nathan Osman <admin@quickmediasolutions.com>
2014-04-13 16:42:36 -0700

    Expanded admin options for articles (closes #45).

--- twobuntu/articles/admin.py
+++ twobuntu/articles/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from twobuntu.articles.models import Article
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    """Describe the administration inferface for articles."""
+
+    date_hierarchy = 'date'
+    list_display   = ('title', 'author', 'date',)
+    list_filter    = ('status', 'cc_license', 'category', 'author',)
+    search_fields  = ('title', 'body',)
+
+admin.site.register(Article, ArticleAdmin)
