31187e5a4d35cf449d16d4fafa22a553ae3983ad
Nathan Osman <admin@quickmediasolutions.com>
2014-04-23 18:32:52 -0700

    Added reCAPTCHA field to the feedback form (closes #54).

--- twobuntu/forms.py
+++ twobuntu/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from captcha.fields import ReCaptchaField
 
 class FeedbackForm(forms.Form):
     """Form for user feedback."""
@@ -7,3 +8,4 @@ class FeedbackForm(forms.Form):
     email = forms.CharField(required=False,
                             help_text="Not required unless you would like a reply.")
     comments = forms.CharField(widget=forms.Textarea())
+    captcha = ReCaptchaField(attrs={'theme': 'clean',})
--- twobuntu/settings.py
+++ twobuntu/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Django helper applications
+    'captcha',
     'south',
     'widget_tweaks',
     # 2buntu applications
