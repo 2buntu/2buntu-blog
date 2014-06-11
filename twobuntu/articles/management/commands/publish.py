1eeb5718a0878071d8e526fa69472357f5647587
Nathan Osman <admin@quickmediasolutions.com>
2014-06-10 17:08:22 -0700

    Added management command to publish all articles scheduled in the past.

---
+++ twobuntu/articles/management/commands/publish.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.utils.timezone import now
+
+from twobuntu.articles.models import Article
+
+class Command(BaseCommand):
+    """Publish all scheduled articles dated in the past."""
+    
+    help = "Publish all scheduled articles."
+
+    def handle(self, *args, **kwargs):
+        """Process the command."""
+        for article in Article.objects.filter(scheduledarticle__date__lte=now()):
+            article.status = Article.PUBLISHED
+            article.save()
+            print '"%s" published.' % article.title
