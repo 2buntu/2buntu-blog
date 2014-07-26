903a24458694bb9e28be1c05d2e4547d91b0a326
Nathan Osman <admin@quickmediasolutions.com>
2014-07-25 19:54:32 -0700

    Updated the shorturls app to conform to PEP8.

--- twobuntu/shorturls/views.py
+++ twobuntu/shorturls/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import get_object_or_404, redirect
 
 from twobuntu.shorturls.models import ShortURL
 
+
 def shorturl(request, key):
-    """Redirect the client."""
+    """
+    Redirect the client.
+    """
     return redirect(get_object_or_404(ShortURL, pk=key).url)
