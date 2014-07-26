bb5a0d789fb77c09e08ca22ff160214743d6234d
Nathan Osman <admin@quickmediasolutions.com>
2014-07-25 19:09:20 -0700

    Updated the categories app to conform to PEP8.

--- twobuntu/categories/models.py
+++ twobuntu/categories/models.py
@@ -1,21 +1,27 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
-class Category(models.Model):
-    """A grouping for articles of a similar topic."""
 
-    name = models.CharField(max_length=40,
-                            help_text="The name of the category.")
+class Category(models.Model):
+    """
+    A grouping for articles of a similar topic.
+    """
+
+    name = models.CharField(
+        max_length=40,
+        help_text="The name of the category.",
+    )
 
     def __unicode__(self):
-        """Return a string representation of the category."""
         return self.name
 
     @models.permalink
     def get_absolute_url(self):
-        """Return the absolute URL of the article."""
+        """
+        Return the absolute URL of the article.
+        """
         return ('categories:view', (), {
-            'id':   self.id,
+            'id': self.id,
             'slug': slugify(self.name),
         })
 
--- twobuntu/categories/views.py
+++ twobuntu/categories/views.py
@@ -4,11 +4,16 @@ from twobuntu.articles.models import Article
 from twobuntu.categories.models import Category
 from twobuntu.decorators import canonical
 
+
 @canonical(Category)
 def view(request, category):
     """Display articles filed under the specified category."""
     return render(request, 'categories/view.html', {
         'title':    category.name,
         'category': category,
-        'articles': Article.objects.select_related('author', 'author__profile', 'category').filter(category=category, status=Article.PUBLISHED),
+        'articles': Article.objects.select_related(
+            'author',
+            'author__profile',
+            'category'
+        ).filter(category=category, status=Article.PUBLISHED),
     })
