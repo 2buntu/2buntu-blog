eb33a6fa56f0e089dbc319c35e0bba8405eb3833
Nathan Osman <admin@quickmediasolutions.com>
2014-08-10 13:02:49 -0700

    Added clearcache management command.

---
+++ twobuntu/articles/management/commands/clearcache.py
@@ -0,0 +1,20 @@
+from django.core.cache import cache
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """
+    Completely clear the cache.
+
+    Do NOT use this command when it is not necessary since it will immediately
+    result in a spike of markdown rendering due to incoming requests.
+    """
+
+    help = "Completely clear the cache."
+
+    def handle(self, *args, **kwargs):
+        """
+        Process the command.
+        """
+        cache.clear()
+        print 'Cache has been completely cleared.'
