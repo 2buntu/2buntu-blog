7aeacb5d1804532a0d36e4baa9c2e72915060e7c
Nathan Osman <admin@quickmediasolutions.com>
2014-08-10 00:53:14 -0700

    Properly implemented a block processor for alert boxes in markdown.

--- twobuntu/cmarkdown.py
+++ twobuntu/cmarkdown.py
@@ -36,7 +36,7 @@ class ImagePattern(markdown.inlinepatterns.Pattern):
         """
         Initialize the pattern.
         """
-        super(ImagePattern, self).__init__(r'\[image:\s*(\d+)\]')
+        super(ImagePattern, self).__init__(r'\[image:\s*(\d+)(:primary)?\]')
 
     def handleMatch(self, m):
         """
@@ -58,25 +58,62 @@ class AlertProcessor(markdown.blockprocessors.BlockProcessor):
     Block processor to display alert boxes.
     """
 
-    TAGS = ['info', 'warning', 'danger']
-    PATTERN = re.compile(r'^\[(%s)\](.*)\[/\1\]$' % '|'.join(TAGS), re.DOTALL)
+    TAGS = {
+        'info': 'info-circle',
+        'warning': 'warning',
+        'danger': 'bomb',
+    }
+    PATTERN = re.compile(r'\[(%s)\]' % '|'.join(TAGS.keys()))
 
     def test(self, parent, block):
         """
         Identify the type of block.
         """
-        return self.PATTERN.match(block)
+        return self.PATTERN.search(block)
 
     def run(self, parent, blocks):
         """
         Perform any necessary substitutions.
         """
-        t, r = self.PATTERN.match(blocks.pop(0)).groups()
-        e = markdown.util.etree.SubElement(parent, 'div')
-        e.set('class', 'alert alert-%s' % t)
-        e.text = r
-        i = markdown.util.etree.SubElement(e, 'span')
-        i.set('class', 'sprite sprite-icon sprite-icon-%s' % t)
+        block = blocks.pop(0)
+        open_match = self.PATTERN.search(block)
+        tag = open_match.group(1)
+        # Have the parser parse everything before the opening tag
+        self.parser.parseBlocks(parent, [block[:open_match.start()]])
+        # Build a list of blocks inside the tag
+        content = [block[open_match.end():]]
+        # Search for the appropriate closing tag
+        close_pattern = re.compile(r'\[/%s\]' % tag)
+        while True:
+            close_match = close_pattern.search(content[-1])
+            if close_match:
+                # Remove the closing tag and trailing text from the last block
+                last_block = content.pop(-1)
+                content.append(last_block[:close_match.start()])
+                # Create the alert element and parse the blocks inside the alert body
+                self.parser.parseBlocks(self._create_alert(parent, tag), content)
+                # Parse the text after the closing tag
+                self.parser.parseBlocks(parent, [last_block[close_match.end():]])
+                break
+            # If closing tag is missing, make it implicit
+            if not len(blocks):
+                content.append('[/%s]' % tag)
+            else:
+                content.append(blocks.pop(0))
+
+    def _create_alert(self, parent, tag):
+        """
+        Create the container for the content.
+        """
+        alert = markdown.util.etree.SubElement(parent, 'div')
+        alert.set('class', 'alert alert-%s' % tag)
+        media = markdown.util.etree.SubElement(alert, 'div')
+        media.set('class', 'media')
+        icon = markdown.util.etree.SubElement(media, 'span')
+        icon.set('class', 'fa fa-4x fa-%s media-object pull-left' % self.TAGS[tag])
+        body = markdown.util.etree.SubElement(media, 'div')
+        body.set('class', 'media-body')
+        return body
 
 
 class TwoBuntuExtension(markdown.Extension):
@@ -92,7 +129,7 @@ class TwoBuntuExtension(markdown.Extension):
         md.inlinePatterns.add('strikethrough', strikethrough_pattern, '_end')
         md.inlinePatterns.add('linkify', LinkifyPattern(), '_end')
         md.inlinePatterns.add('image', ImagePattern(), '_end')
-        md.parser.blockprocessors.add('alert', AlertProcessor(md.parser), '>indent')
+        md.parser.blockprocessors.add('alert', AlertProcessor(md.parser), '_begin')
 
 
 cm = markdown.Markdown(safe_mode='escape', extensions=[
