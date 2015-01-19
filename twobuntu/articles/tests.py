f101dbf65ceedec990770d41c99fb0ffd7218e4d
Nathan Osman <nathan@quickmediasolutions.com>
2015-01-18 16:24:07 -0800

    Added utility functions for creating 'dummy' items.

--- twobuntu/articles/tests.py
+++ twobuntu/articles/tests.py
@@ -1,12 +1,11 @@
-from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.auth.models import AnonymousUser
 from django.http import Http404
 from django.test import TestCase
 from django.test.client import RequestFactory
-from django.utils.timezone import now
 
 from twobuntu.articles.models import Article
 from twobuntu.articles.views import view as view_article
-from twobuntu.categories.models import Category
+from twobuntu.utils import dummy_article, dummy_category, dummy_user
 
 
 class TestArticlePermission(TestCase):
@@ -18,28 +17,23 @@ class TestArticlePermission(TestCase):
         # Create the request factory
         self.factory = RequestFactory()
         # Create three users - two normal users and an admin (staff)
-        self.user1 = User.objects.create_user('user1', 'user1@example.com', 'user1')
-        self.user2 = User.objects.create_user('user2', 'user1@example.com', 'user2')
-        self.admin = User.objects.create_user('admin', 'admin@example.com', 'admin')
-        self.admin.is_staff = True
-        self.admin.save()
-        # Create a dummy category for the articles
-        self.category = Category(name='Test')
-        self.category.save()
+        self.user1 = dummy_user()
+        self.user2 = dummy_user()
+        self.admin = dummy_user(True)
         # Create a dummy article for testing
-        self.article = Article(author=self.user1, category=self.category, title='Test', body='Test', date=now())
+        self.article = dummy_article(self.user1, dummy_category())
 
-    def request(self, article, user):
-        request = self.factory.get(article.get_absolute_url())
+    def request(self, user):
+        request = self.factory.get(self.article.get_absolute_url())
         request.user = user
-        return view_article(request, article.id)
+        return view_article(request, self.article.id)
 
     def check_permission(self, user, access):
         if access:
-            self.assertEqual(self.request(self.article, user).status_code, 200)
+            self.assertEqual(self.request(user).status_code, 200)
         else:
             with self.assertRaises(Http404):
-                self.request(self.article, user)
+                self.request(user)
 
     def test_access_to_draft_article(self):
         self.article.status = Article.DRAFT
--- twobuntu/utils.py
+++ twobuntu/utils.py
@@ -1,5 +1,11 @@
 from uuid import uuid4
 
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+
+from twobuntu.articles.models import Article
+from twobuntu.categories.models import Category
+
 
 def uuid():
     """
@@ -13,3 +19,41 @@ def uuid6():
     Generate a UUID and truncate it to six characters.
     """
     return uuid()[:6]
+
+
+def dummy_user(staff=False):
+    """
+    Generate a dummy user for testing.
+    """
+    if staff:
+        return User.objects.create_superuser(
+            uuid6(),
+            'user@example.com',
+            'password',
+        )
+    else:
+        return User.objects.create_user(uuid6())
+
+
+def dummy_category():
+    """
+    Generate a dummy category.
+    """
+    category = Category(name=uuid6())
+    category.save()
+    return category
+
+
+def dummy_article(author, category):
+    """
+    Generate a dummy article for testing.
+    """
+    article = Article(
+        author=author,
+        category=category,
+        title=uuid6(),
+        body=uuid6(),
+        date=now(),
+    )
+    article.save()
+    return article
