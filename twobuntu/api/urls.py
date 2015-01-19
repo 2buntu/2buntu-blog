8165d3fe27649af8afaef4a45849a6da126fd51a
Nathan Osman <nathan@quickmediasolutions.com>
2015-01-18 18:16:16 -0800

    Added names to API views.

--- twobuntu/api/urls.py
+++ twobuntu/api/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import include, patterns, url
 urlpatterns = patterns('',
     # Version 1.2 is the current version
     url(r'^$', 'twobuntu.api.v1_2.views.index', name='index'),
-    url(r'^1.2/', include('twobuntu.api.v1_2.urls')),
+    url(r'^1.2/', include('twobuntu.api.v1_2.urls', '1.2')),
 )
--- twobuntu/api/v1_2/urls.py
+++ twobuntu/api/v1_2/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('twobuntu.api.v1_2.views',
-    url(r'^articles/$', 'articles'),
-    url(r'^articles/(?P<id>\d+)/$', 'article_by_id'),
+    url(r'^articles/$', 'articles', name='articles'),
+    url(r'^articles/(?P<id>\d+)/$', 'article_by_id', name='article_by_id'),
 
-    url(r'^authors/$', 'authors'),
-    url(r'^authors/(?P<id>\d+)/$', 'author_by_id'),
-    url(r'^authors/(?P<id>\d+)/articles/$', 'articles_by_author'),
+    url(r'^authors/$', 'authors', name='authors'),
+    url(r'^authors/(?P<id>\d+)/$', 'author_by_id', name='author_by_id'),
+    url(r'^authors/(?P<id>\d+)/articles/$', 'articles_by_author', name='articles_by_author'),
 
-    url(r'^categories/$', 'categories'),
-    url(r'^categories/(?P<id>\d+)/articles/$', 'articles_by_category'),
+    url(r'^categories/$', 'categories', name='categories'),
+    url(r'^categories/(?P<id>\d+)/articles/$', 'articles_by_category', name='articles_by_category'),
 )
