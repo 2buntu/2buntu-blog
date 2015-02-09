c37fb507778b12311ba0db744ac19eff7dda48b2
Nathan Osman <nathan@quickmediasolutions.com>
2015-02-08 23:14:37 -0800

    Added test for min / max parameters in the API (#21).

--- twobuntu/api/v1_2/tests.py
+++ twobuntu/api/v1_2/tests.py
@@ -1,8 +1,10 @@
+from calendar import timegm
 from json import loads
 
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.utils.encoding import smart_text
+from django.utils.timezone import now
 
 from twobuntu.articles.models import Article
 from twobuntu.utils import dummy_article, dummy_category, dummy_user
@@ -19,18 +21,24 @@ class TestAPI(TestCase):
         self.category = dummy_category()
         self.article = dummy_article(self.user, self.category, Article.PUBLISHED)
 
-    def get_response(self, view, expected=1, **kwargs):
-        url = reverse(view, kwargs=kwargs)
-        response = loads(smart_text(self.client.get(url).content))
+    def get_response(self, view, viewargs={}, getargs={}, expected=1):
+        url = reverse(view, kwargs=viewargs)
+        response = loads(smart_text(self.client.get(url, data=getargs).content))
         self.assertEqual(len(response), expected)
         return response
 
     def test_methods(self):
-        # Ensure all of the methods return a single response when appropriate
         self.get_response('api:1.2:articles')
-        self.get_response('api:1.2:article_by_id', id=self.article.id)
+        self.get_response('api:1.2:article_by_id', viewargs={'id': self.article.id})
         self.get_response('api:1.2:authors')
-        self.get_response('api:1.2:author_by_id', id=self.user.id)
-        self.get_response('api:1.2:articles_by_author', id=self.user.id)
+        self.get_response('api:1.2:author_by_id', viewargs={'id': self.user.id})
+        self.get_response('api:1.2:articles_by_author', viewargs={'id': self.user.id})
         self.get_response('api:1.2:categories')
-        self.get_response('api:1.2:articles_by_category', id=self.category.id)
+        self.get_response('api:1.2:articles_by_category', viewargs={'id': self.category.id})
+
+    def test_min_max(self):
+        past, future = 1, timegm(now().utctimetuple()) + 1
+        self.get_response('api:1.2:articles', getargs={'min': past})
+        self.get_response('api:1.2:articles', getargs={'max': future})
+        self.get_response('api:1.2:articles', expected=0, getargs={'min': future})
+        self.get_response('api:1.2:articles', expected=0, getargs={'max': past})
