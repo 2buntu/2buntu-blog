b9484532e57da03401ab293fad4e05be98d21d7e
Nathan Osman <nathan@quickmediasolutions.com>
2015-02-09 00:15:20 -0800

    Completed tests for the API (#21).

--- twobuntu/api/v1_2/tests.py
+++ twobuntu/api/v1_2/tests.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from json import loads
 
 from django.core.urlresolvers import reverse
@@ -15,29 +16,49 @@ class TestAPI(TestCase):
     """
 
     def setUp(self):
-        # Create a dummy user, category, and article for testing
+        # Create a dummy user, category, and two articles for testing
         self.user = dummy_user()
         self.category = dummy_category()
-        self.article = dummy_article(self.user, self.category, Article.PUBLISHED)
+        self.article1 = dummy_article(self.user, self.category, Article.PUBLISHED)
+        self.article2 = dummy_article(self.user, self.category, Article.PUBLISHED)
+        # Make the second article older so that the first one always sorts first
+        self.article2.date -= timedelta(0, 1)
+        self.article2.save()
 
-    def get_response(self, view, viewargs={}, getargs={}, expected=1):
+    def check_response(self, num, view, viewargs={}, getargs={}):
         url = reverse(view, kwargs=viewargs)
         response = loads(smart_text(self.client.get(url, data=getargs).content))
-        self.assertEqual(len(response), expected)
+        self.assertEqual(len(response), num)
         return response
 
+    def check_error(self, view, **kwargs):
+        self.assertTrue('error' in self.check_response(1, view, **kwargs))
+
     def test_methods(self):
-        self.get_response('api:1.2:articles')
-        self.get_response('api:1.2:article_by_id', viewargs={'id': self.article.id})
-        self.get_response('api:1.2:authors')
-        self.get_response('api:1.2:author_by_id', viewargs={'id': self.user.id})
-        self.get_response('api:1.2:articles_by_author', viewargs={'id': self.user.id})
-        self.get_response('api:1.2:categories')
-        self.get_response('api:1.2:articles_by_category', viewargs={'id': self.category.id})
+        self.check_response(2, 'api:1.2:articles')
+        self.check_response(1, 'api:1.2:article_by_id', viewargs={'id': self.article1.id})
+        self.check_response(1, 'api:1.2:authors')
+        self.check_response(1, 'api:1.2:author_by_id', viewargs={'id': self.user.id})
+        self.check_response(2, 'api:1.2:articles_by_author', viewargs={'id': self.user.id})
+        self.check_response(1, 'api:1.2:categories')
+        self.check_response(2, 'api:1.2:articles_by_category', viewargs={'id': self.category.id})
+
+    def test_page_size(self):
+        self.check_response(1, 'api:1.2:articles', getargs={'size': 1})
+        self.check_response(1, 'api:1.2:articles', getargs={'page': 2, 'size': 1})
+        self.check_response(0, 'api:1.2:articles', getargs={'page': 3, 'size': 1})
 
     def test_min_max(self):
         past, future = 1, datetime_to_timestamp(now()) + 1
-        self.get_response('api:1.2:articles', getargs={'min': past})
-        self.get_response('api:1.2:articles', getargs={'max': future})
-        self.get_response('api:1.2:articles', expected=0, getargs={'min': future})
-        self.get_response('api:1.2:articles', expected=0, getargs={'max': past})
+        self.check_response(2, 'api:1.2:articles', getargs={'min': past})
+        self.check_response(2, 'api:1.2:articles', getargs={'max': future})
+        self.check_response(0, 'api:1.2:articles', getargs={'min': future})
+        self.check_response(0, 'api:1.2:articles', getargs={'max': past})
+
+    def test_bad_input(self):
+        for arg in ('min', 'max', 'page', 'size'):
+            self.check_error('api:1.2:articles', getargs=dict(((arg, '-'),)))
+
+    def test_jsonp(self):
+        response = self.client.get(reverse('api:1.2:articles'), data={'callback': 'test'})
+        self.assertEqual(smart_text(response.content)[0:5], 'test(')
