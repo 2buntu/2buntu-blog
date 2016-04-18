6d4a52ac113df766246afd52a506ebffcd13ecd3
Nathan Osman <nathan@quickmediasolutions.com>
2016-04-17 17:45:45 -0700

    Completely removed ads code.

--- twobuntu/ads/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from twobuntu.ads.models import Ad
-
-admin.site.register(Ad)
--- twobuntu/ads/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('twobuntu.ads.views',
-    url(r'^$', 'index', name='index'),
-)
