2077b8c89912658f82dea10e7a04c001e7665812
Nathan Osman <nathan@quickmediasolutions.com>
2016-08-28 22:10:10 -0700

    Update context_processors to work with Django 1.10.

--- twobuntu/settings.py
+++ twobuntu/settings.py
@@ -39,13 +39,13 @@ TEMPLATES = (
             'context_processors': (
                 # Core Django context processors
                 'django.contrib.auth.context_processors.auth',
-                'django.core.context_processors.debug',
-                'django.core.context_processors.i18n',
-                'django.core.context_processors.media',
-                'django.core.context_processors.request',
-                'django.core.context_processors.static',
-                'django.core.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.request',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
                 # 2buntu context processors
                 'twobuntu.context_processors.read_only',
             ),
